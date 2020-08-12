9c934d4909a54d7d610c43f86e99f31424b23172
Ian Mok <ian_22@live.hk>
2020-08-12 17:55:16 +0800

    Initial commit

---
+++ Plugin/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Xrm.Sdk;
+
+namespace Plugin
+{
+    public class TestPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                // execute logic here...
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
