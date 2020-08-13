f62ac8f3b9ea37627cb8ae5ea14586dc7e43844c
Ian Mok <ian_22@live.hk>
2020-08-13 10:27:26 +0800

    add lowContactScoreAlert when onload in contact js

---
+++ Plugin/Contact_CalculateContactScore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Xrm.Sdk;
+
+namespace Plugin
+{
+    public class Contact_CalculateContactScore : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                // execute logic here...
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ WebResource/fph_contact.js
@@ -0,0 +1,12 @@
+﻿var Handler = {}
+
+Handler.OnLoad = function (ctx) {
+    lowContactScoreAlert(ctx)
+}
+
+function lowContactScoreAlert(ctx) {
+    var contactScore = Xrm.Page.getAttribute("fph_contactscore").getValue()
+    if (contactScore < 50) {
+        alert("Contact Score is below 50. Please input more contact information!")
+    }
+}
